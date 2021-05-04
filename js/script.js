ede09c0910a79cc89829733aa1aefbcede87cb63
DarkFlamingo <mgavatuk@gmail.com>
2021-05-04 22:12:11 +0300

    Add particles background.

---
+++ js/script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+  particlesJS.load(
+    "particles-js",
+    "assets/particles/config.json",
+    function () {
+      console.log("callback");
+    }
+  );
+
+  window.addEventListener("scroll", function () {
+    let last_known_scroll_position = window.scrollY;
+    let max_height = window.innerWidth;
+    if (
+      last_known_scroll_position >= 0 &&
+      last_known_scroll_position < max_height * (1 / 4)
+    ) {
+      let el = document.getElementById("particles-js");
+      el.className = "bg-color-f";
+    } else if (
+      last_known_scroll_position > max_height * (1 / 4) &&
+      last_known_scroll_position < max_height * (1 / 2)
+    ) {
+      let el = document.getElementById("particles-js");
+      el.className = "bg-color-f2";
+    } else {
+      let el = document.getElementById("particles-js");
+      el.className = "bg-color-f3";
+    }
+  });
+});
